ea813c1b1bf9eb9ba432db709aaf1d85aa51d398
joaooliveiradev <joaooliveira1775@gmail.com>
2021-09-01 19:48:23 -0300

    subindo arquivos da aula 9

---
+++ aula-009/my-app/src/app.js
@@ -0,0 +1,9 @@
+function App() {
+    function handleClick() {
+        alert('clicou');
+    }
+    return (
+        <button onClick={handleClick}>Clique aqui</button>
+    )
+}
+export default App;
